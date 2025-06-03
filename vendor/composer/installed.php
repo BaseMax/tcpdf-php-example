a35eb7c49fb9bbbe6390c935d1af3d1d7de382dc
Max Base <maxbasecode@gmail.com>
2025-06-03 00:36:22 +0000

    add vendor git push

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'basemax/tcpdf-php-example',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a382fa1c087a037afe668fd531e407cdd3da89a1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'basemax/tcpdf-php-example' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a382fa1c087a037afe668fd531e407cdd3da89a1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'ca5b6de294512145db96bcbc94e61696599c391d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
